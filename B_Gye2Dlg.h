461a4e8dc20c4ee2f6b8ca7696e5c70ff2d69b6c
권윤호 <yoonhoktx@gmail.com>
2023-10-23 10:04:15 +0900

    초기 커밋.

---
+++ B_Gye2Dlg.h
@@ -0,0 +1,43 @@
+﻿
+// B_Gye2Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CBGye2Dlg 대화 상자
+class CBGye2Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CBGye2Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_B_GYE2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_a;
+	int m_b;
+	int m_c;
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton4();
+	afx_msg void OnEnChangeEdit2();
+};
